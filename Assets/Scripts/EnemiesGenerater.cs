09cc2f93e1d8673a51f46e90c63d3b516157cabf
yamagi208 <scb01169@gmail.com>
2020-03-09 14:13:33 +0900

    敵の生成時の速さ、enemyNumber、待機時間のインスペクタービュー上での変数化

--- Assets/Scripts/EnemiesGenerater.cs
+++ Assets/Scripts/EnemiesGenerater.cs
@@ -9,6 +9,9 @@ public class EnemiesGenerater : MonoBehaviour
     public GameObject enemiesFolder;
     static int enemyNumber;
 
+    public float minWaitTime,maxWaitTime,minSpeedX,maxSpeedX,minSpeedZ,maxSpeedZ;
+    public int minEnemyNumber,maxEnemyNumber;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,12 +27,9 @@ public class EnemiesGenerater : MonoBehaviour
     {
         for(int n = 1;n != 0; n++ )
         {
-            //Debug.Log(+ n + "回目開始");
-
             //ランダム時間の待機
-            float waitTime=Random.Range(1f,3f);
+            float waitTime=Random.Range(minWaitTime,maxWaitTime);
             yield return new WaitForSeconds(waitTime);
-            //Debug.Log(+ n + "回目待機:" + waitTime + "秒終了");
 
             //エネミーをenemiesFolder下に生成
             GameObject enemyPrefab = enemyPrefabs[Random.Range(0,enemyPrefabs.Length)];
@@ -37,21 +37,16 @@ public class EnemiesGenerater : MonoBehaviour
             enemy.transform.parent = enemiesFolder.transform;
             
             //エネミーの数値決定
-            enemyNumber = Random.Range(0,6);
+            enemyNumber = Random.Range(minEnemyNumber,maxEnemyNumber);
             enemy.transform.Find("Canvas").Find("EnemyNumber").gameObject.GetComponent<Text>().text = "" + enemyNumber;
-
-            /*To成宮
-            生成したenemy下にint型のenemyNumberで数値を与えて流から、enemyNumberを取得すれば大丈夫なはず。加減乗除は与えてないので接触する敵から判断するようにしてほしい。
-            */
-            //Debug.Log(enemyNumber);
             
             //エネミーにランダム方向に力を加える
             Rigidbody enemyRigidbody = enemy.GetComponent<Rigidbody>();
-            enemyRigidbody.AddForce(new Vector3(Random.Range(-10f,10f),0,Random.Range(-10f,10f)) , ForceMode.VelocityChange);
+            enemyRigidbody.AddForce(new Vector3(Random.Range(minSpeedX,maxSpeedX),0,Random.Range(minSpeedZ,maxSpeedZ)) , ForceMode.VelocityChange);
 
+            //何かしらの条件を満たしたら敵の生成を終了
             if(n==5)
             {
-                //Debug.Log("コルーチン終了");
                 yield break;
             }
         }        
